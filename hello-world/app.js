c2dd37eb3a09284478852fc6ccc8dad24c6551c7
Oscar Muñoz <cok.kike@gmail.com>
2020-10-28 23:11:25 -0600

    Test error deploy

--- hello-world/app.js
+++ hello-world/app.js
@@ -15,8 +15,7 @@ let response;
  * 
  */
 exports.lambdaHandler = async (event, context) => {
-    throw new Error("This will cause a deployment rollback");
-    
+    throw new Error("This will cause a deployment rollback");    
     // try {
     //     // const ret = await axios(url);
     //     console.info('hello world lambda......');
